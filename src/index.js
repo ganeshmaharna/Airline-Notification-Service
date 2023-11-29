9238a43324ac4032c6dfaaa589d632d3536a48a7
Ganesh Maharna <ganeshmaharna@gmail.com>
2023-11-29 10:51:23 +0530

    Created the nodemailer basic setup and tried to send mail using index file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const mailsender = require("./config/email-config")
 
 const app = express();
 app.use(express.json());
@@ -11,4 +12,15 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async() => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    try {
+        const response = await mailsender.sendMail({
+            from: ServerConfig.GMAIL_EMAIL,
+            to: 'ganeshmaharna76@gmail.com',
+            subject: 'Is the service working ? now as well',
+            text: 'Yes it is working'
+        });
+        console.log(response);
+    } catch(error) {
+        console.log("This is the error ",error);
+    }
 });
